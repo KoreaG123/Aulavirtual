2676d399c2fc9f8a97960bbaf542065f26cb466b
KoreaG123 <90579765+KoreaG123@users.noreply.github.com>
2026-01-16 19:25:58 -0500

    Update alumno.js

--- js/alumno.js
+++ js/alumno.js
@@ -1,1 +1,19 @@
 
+document.addEventListener("DOMContentLoaded", async () => {
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+
+  auth.onAuthStateChanged(async (user) => {
+    if (!user) return;
+
+    const doc = await db.collection("users").doc(user.uid).get();
+
+    if (!doc.exists || doc.data().role !== "alumno") {
+      alert("Acceso denegado");
+      window.location.href = "index.html";
+      return;
+    }
+
+    console.log("Alumno autorizado");
+  });
+});
