38a036834f0b8610f33bae7a0d0a6d673769941e
KoreaG123 <90579765+KoreaG123@users.noreply.github.com>
2026-01-16 22:07:14 -0500

    Update role.js

--- js/role.js
+++ js/role.js
@@ -1,39 +1,30 @@
-
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener("DOMContentLoaded", () => {
   const auth = firebase.auth();
   const db = firebase.firestore();
 
   auth.onAuthStateChanged(async (user) => {
     if (!user) {
-      window.location.href = "index.html";
+      location.href = "index.html";
       return;
     }
 
     const doc = await db.collection("users").doc(user.uid).get();
-
     if (!doc.exists) {
       auth.signOut();
-      window.location.href = "index.html";
+      location.href = "index.html";
       return;
     }
 
     const role = doc.data().role;
+    const page = location.pathname;
 
-    const page = window.location.pathname;
+    if (role === "admin" && !page.includes("admin.html"))
+      location.href = "admin.html";
 
-    // ADMIN
-    if (role === "admin" && !page.includes("admin.html")) {
-      window.location.href = "admin.html";
-    }
+    if (role === "profesor" && !page.includes("profesor.html"))
+      location.href = "profesor.html";
 
-    // PROFESOR
-    if (role === "profesor" && !page.includes("profesor.html")) {
-      window.location.href = "profesor.html";
-    }
-
-    // ALUMNO
-    if (role === "alumno" && !page.includes("alumno.html")) {
-      window.location.href = "alumno.html";
-    }
+    if (role === "alumno" && !page.includes("alumno.html"))
+      location.href = "alumno.html";
   });
 });
