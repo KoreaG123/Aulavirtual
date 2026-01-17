468fa6fb0d8c93258c4bd3921750988f09bb8bd3
KoreaG123 <90579765+KoreaG123@users.noreply.github.com>
2026-01-16 19:03:54 -0500

    Update role.js

--- js/role.js
+++ js/role.js
@@ -1,1 +1,39 @@
 
+document.addEventListener("DOMContentLoaded", async () => {
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+
+  auth.onAuthStateChanged(async (user) => {
+    if (!user) {
+      window.location.href = "index.html";
+      return;
+    }
+
+    const doc = await db.collection("users").doc(user.uid).get();
+
+    if (!doc.exists) {
+      auth.signOut();
+      window.location.href = "index.html";
+      return;
+    }
+
+    const role = doc.data().role;
+
+    const page = window.location.pathname;
+
+    // ADMIN
+    if (role === "admin" && !page.includes("admin.html")) {
+      window.location.href = "admin.html";
+    }
+
+    // PROFESOR
+    if (role === "profesor" && !page.includes("profesor.html")) {
+      window.location.href = "profesor.html";
+    }
+
+    // ALUMNO
+    if (role === "alumno" && !page.includes("alumno.html")) {
+      window.location.href = "alumno.html";
+    }
+  });
+});
