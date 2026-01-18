4771d8ab5961a20a24dcf9e33bd383f58dc4a9b1
KoreaG123 <90579765+KoreaG123@users.noreply.github.com>
2026-01-18 15:51:53 -0500

    Update role.js

--- js/role.js
+++ js/role.js
@@ -9,22 +9,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const doc = await db.collection("users").doc(user.uid).get();
+
     if (!doc.exists) {
-      auth.signOut();
+      await auth.signOut();
       location.href = "index.html";
       return;
     }
 
-    const role = doc.data().role;
-    const page = location.pathname;
-
-    if (role === "admin" && !page.includes("admin.html"))
-      location.href = "admin.html";
-
-    if (role === "profesor" && !page.includes("profesor.html"))
-      location.href = "profesor.html";
-
-    if (role === "alumno" && !page.includes("alumno.html"))
-      location.href = "alumno.html";
+    // ✅ SOLO validar sesión
+    // ❌ NO redirigir aquí
   });
 });
