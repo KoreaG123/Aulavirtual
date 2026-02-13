9ee0b7081de6c610bd9caa9e2564ad45238de726
KoreaG123 <90579765+KoreaG123@users.noreply.github.com>
2026-02-13 06:29:44 -0500

    Update Firebase configuration details

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,11 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBYF_5PJHaClW8h4lwl_EXH5gSZqU57Xyk",
-  authDomain: "aulavirtual-13e8d.firebaseapp.com",
-  projectId: "aulavirtual-13e8d",
-  storageBucket: "aulavirtual-13e8d.appspot.com",
-  messagingSenderId: "1042049492955",
-  appId: "1:1042049492955:web:e0a2e90a64528d705129fe"
+  apiKey: "AIzaMirones",
+  authDomain: "aula",
+  projectId: "aulavir",
+  storageBucket: "aula",
+  messagingSenderId: "10",
+  appId: "1:10"
 };
 
 firebase.initializeApp(firebaseConfig);
